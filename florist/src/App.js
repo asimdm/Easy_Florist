10c733a7f6cb2f3ab0278ff301fc14b9dbf1ad5e
asimdm <asimalidm.2020cs@technonjr.org>
2023-08-30 17:08:08 +0530

    products added

--- florist/src/App.js
+++ florist/src/App.js
@@ -1,3 +1,5 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
@@ -5,7 +7,22 @@ function App() {
         <a href='/'>Easy Florist</a>
       </header>
       <main>
-        Flowers
+        <h3>Popular Bouquet</h3>
+        <div className="products">
+        {
+          data.products.map(product => (
+            <div className="product" key={product.productId}>
+              <a style={{textDecoration:'none',color:'black'}} href={`/product/${product.productId}`}>
+                <img src={product.image} alt="bouquet" />
+                <div className="product-info">
+                  <p>{product.name}</p>
+                  <p><strong>₹{product.price}</strong></p>
+                </div>
+              </a>
+            </div>
+          ))
+        }
+        </div>
       </main>
     </div>
   );
---
+++ florist/src/data.js
@@ -0,0 +1,61 @@
+const data = {
+    products:[
+        {
+            productId: 1,
+            name: 'Midnight Blue Orchids',
+            slug: 'midnight-blue-orchid',
+            price: 850,
+            category: 'orchids',
+            image: '/images/midnight-orchid.png',
+            countInStock: 30,
+            rating: 5,
+            numReviews: 15,
+            description: 'The most highly coveted of ornamental plants the delicate, exotic and graceful orchid represents love, luxury, beauty and strength. Mesmerize your loved ones with this luxurious orchid bouquet with Asparagus Fillers.',
+            productDetails: [
+                '6 Blue Orchids',
+                'Green Delhi Asparagus Fillers',
+                'Sky Blue Non Woven Paper Wrapping',
+                'Beautifully Tied With White Ribbon'
+            ],
+        },
+        {
+            productId: 2,
+            name: 'Soothing White Carnations',
+            slug: 'soothing-white-carnations',
+            price: 850,
+            category: 'carnations',
+            image: '/images/white-carnations.png',
+            countInStock: 10,
+            rating: 4.5,
+            numReviews: 15,
+            description: "The pure, facinationg and royal God's Flower represents devotion, love and fascination. Enchant your other half with this calming carnations bouquet with WGreen Goldenrod Fillers.",
+            productDetails: [
+                '8 White Carnations',
+                'Green Goldenrod Fillers',
+                'Wrapped in White Paper',
+                'Tied with White Ribbon',
+            ],
+        },
+        {
+            productId: 3,
+            name: 'Timeless Red Roses',
+            slug: 'timeless-red-roses',
+            price: 750,
+            category: 'roses',
+            image: '/images/red-roses.png',
+            countInStock: 50,
+            rating: 5,
+            numReviews: 15,
+            description: 'The classic beauty of flowers the romantic and passionate rose represents love, hope, gratitude, passion and compassion. Bring your loved ones this classic red rose bouquet with White Gypsy Flower Fillers, to show your love.',
+            productDetails: [
+                '10 Red Roses',
+                'White Gypsy Flower Fillers',
+                'Beautifully Wrapped with Non-woven Red Paper',
+                'FNP Brown Strip Paper Packing',
+                'Tied with Black Satin Ribbon',
+            ],
+        },
+    ]
+}
+
+export default data;
