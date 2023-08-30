7fecfc02e0ed03e6a1ffd705f7950c2991c50a18
asimdm <asimalidm.2020cs@technonjr.org>
2023-08-30 14:19:33 +0530

    App created

---
+++ florist/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>Easy Florist</a>
+      </header>
+      <main>
+        Flowers
+      </main>
+    </div>
+  );
+}
+
+export default App;
