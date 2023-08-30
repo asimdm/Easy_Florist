a25a7d2edf50f96cf3f91b704ce88030655e8d90
asimdm <asimalidm.2020cs@technonjr.org>
2023-08-30 21:42:07 +0530

    state added

--- florist/src/pages/HomePage.js
+++ florist/src/pages/HomePage.js
@@ -1,26 +1,67 @@
+import { useEffect, useReducer } from "react";
 import { Link } from "react-router-dom";
-import data from "../data";
+import axios from "axios";
+/* import data from "../data"; */
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_ERROR":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function HomePage() {
+  /* const [products, setProduct] = useState([]); */
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: "",
+    products: [],
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_ERROR", payload: err.message });
+      }
+
+      //setProduct(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h3>Popular Bouquet</h3>
       <div className="products">
-        {data.products.map((product) => (
-          <div className="product" key={product.productId}>
-            <Link
-              style={{ textDecoration: "none", color: "black" }}
-              to={`/product/${product.productId}`}>
-              <img src={product.image} alt="bouquet" />
-              <div className="product-info">
-                <p>{product.name}</p>
-                <p>
-                  <strong>₹{product.price}</strong>
-                </p>
-              </div>
-            </Link>
-          </div>
-        ))}
+        {loading ? (
+          <div>Loading....</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
+            <div className="product" key={product.productId}>
+              <Link
+                style={{ textDecoration: "none", color: "black" }}
+                to={`/product/${product.productId}`}>
+                <img src={product.image} alt="bouquet" />
+                <div className="product-info">
+                  <p>{product.name}</p>
+                  <p>
+                    <strong>₹{product.price}</strong>
+                  </p>
+                </div>
+              </Link>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
