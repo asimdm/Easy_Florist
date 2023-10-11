c457851a64b7d07a32e436a5079a258381b27e08
asimdm <asimalidm.2020cs@technonjr.org>
2023-10-11 23:26:42 +0530

    banner change

--- florist/src/component/Banner.js
+++ florist/src/component/Banner.js
@@ -11,10 +11,10 @@ function Banner() {
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <img src='https://img.freepik.com/free-photo/natures-beauty-captured-colorful-flower-close-up-generative-ai_188544-8593.jpg' alt='banner' width={'100%'} height={'500px'}></img>
+        <img src='https://images.pexels.com/photos/268261/pexels-photo-268261.jpeg?cs=srgb&dl=pexels-pok-rie-268261.jpg&fm=jpg' alt='banner' width={'100%'} height={'500px'}></img>
       </Carousel.Item>
       <Carousel.Item>
-      <img src='https://img.freepik.com/free-photo/natures-beauty-captured-colorful-flower-close-up-generative-ai_188544-8593.jpg' alt='banner' width={'100%'} height={'500px'}></img>
+      <img src='https://images6.alphacoders.com/662/662213.jpg' alt='banner' width={'100%'} height={'500px'}></img>
       </Carousel.Item>
       <Carousel.Item>
       <img src='https://img.freepik.com/free-photo/natures-beauty-captured-colorful-flower-close-up-generative-ai_188544-8593.jpg' alt='banner' width={'100%'} height={'500px'}></img>
