aa7fa145f7de266185610cd26517cb8497b44580
asimdm <asimalidm.2020cs@technonjr.org>
2023-10-21 21:07:37 +0530

    new landing page

--- florist/src/App.js
+++ florist/src/App.js
@@ -1,56 +1,15 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ProductPage from "./pages/ProductPage";
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
-import { LinkContainer } from "react-router-bootstrap";
-import Nav from 'react-bootstrap/Nav';
-import Badge from 'react-bootstrap/Badge';
-import { useContext } from "react";
-import { Store } from "./Store";
-
 function App() {
-  const {state} = useContext(Store);
-  const {cart} = state;
   return (
-    <BrowserRouter>
-      <div className="d-flex flex-column site-container">
-        <header>
-          <Navbar>
-            <Container>
-              <LinkContainer to="/">
-                <Navbar.Brand>Easy Florist</Navbar.Brand>
-              </LinkContainer>
-              <Nav className="me-auto">
-                <Link to={"/cart"} className="nav-link">
-                  Cart
-                  {cart.cartItems.length > 0 && (
-                    <Badge pill bg="danger">
-                      {cart.cartItems.length}
-                    </Badge>
-                  )}
-                </Link>
-              </Nav>
-            </Container>
-          </Navbar>
-        </header>
-        <main>
-          <Container className="mt-3">
-            <Routes>
-              <Route path="/products/id/:productId" element={<ProductPage />} />
-              <Route path="/" element={<HomePage />} />
-            </Routes>
-          </Container>
-        </main>
-        <footer>
-          <Container>
-            <div className="text-center">
-              <p>All Rights Reserved</p>
-            </div>
-          </Container>
-        </footer>
+    <div style={{ backgroundColor: "black" }}>
+      <div className="background">
+        <h1 className="text showup">Easy Florist</h1>
+        <div>
+          <h1 className="text reveal">
+            <span className="slidein">The garden of your dreams</span>
+          </h1>
+        </div>
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
