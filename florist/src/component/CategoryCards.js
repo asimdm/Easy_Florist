286dc83aea378ef65a198a28136f2ba4e74f9ff6
asimdm <asimalidm.2020cs@technonjr.org>
2023-11-14 21:03:19 +0530

    css

--- florist/src/component/CategoryCards.js
+++ florist/src/component/CategoryCards.js
@@ -64,8 +64,8 @@ function CategoryCards(){
         <Row>
             {category.map((cat)=>(
           <Col sm={6} md={4} lg={3}>
-            <Link to={`/home`}>
-              <Card className="text-center p-2 m-1">
+            <Link to={`/home`}  style={{ textDecoration: 'none'}}>
+              <Card className="text-center p-2 m-1" key={cat}>
                 <Card.Img
                   variant="top"
                   src="https://i0.wp.com/beato.com.sg/wp-content/uploads/2022/07/How-to-keep-flowers-fresh-after-buying-scaled.jpg?fit=2560%2C1707&ssl=1"
